edfc8c6233a392d01e7995898b8e0231c17efc68
CucciPro <josej@email.arizona.edu>
2020-07-04 23:52:18 -0700

    setup functions for saving score

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,14 @@ var questions = [
     { q: "Arrays in Javascript can be used to store________.", o1: "numbers and strings", o2: "other arrays", o3: "booleans", o4: "all of the above", a: "all of the above" },
     { q: "String values must be enclosed within ________ when being assigned to variables.", o1: "parenthesis", o2: "quotes", o3: "good friends", o4: "all of the above", a: "quotes" },
     { q: "A very useful tool used during the development and debugging for printing content to the debugger is:", o1: "JavaScript", o2: "terminal/bash", o3: "for loops", o4: "console.og", a: "for loops" },
-]
+];
 
 var currentQ = 0;
 var score = [];
 var timer = 60;
 var totalQ = questions.length;
-var correct = ""
+var correct = "";
+var userScore = 0;
 
 function timerStart() {
     var time = function() {
@@ -33,6 +34,13 @@ function timerStart() {
             document.getElementById("alert").innerHTML = "<p>Times Up!</p>";
             clearInterval(counter);
         }
+        else if (currentQ === totalQ) {
+            document.getElementById("question").innerHTML = "<h3>All done!</h3>";
+            document.getElementById("answers").innerHTML = "<p>Your final score is " + userScore + "</p><br/>";
+            document.getElementById("alert").innerHTML = "<p>Enter initials: <input type='text' id='text1'></input><button id='submit'>Submit</button> </p>";
+            document.getElementById("submit").addEventListener("click", highScore);
+            clearInterval(counter);
+        }
     };
 
     if (timer === 60) {
@@ -40,16 +48,16 @@ function timerStart() {
     } 
 };
 
-function hideAnswers() {
-
-    var hide = document.getElementById("answers");
+function showAnswers() {
+    var hide = document.getElementById("buttons");
 
     if (hide.style.visibility === "hidden") {
             hide.style.visibility = "visible";
           } else {
             hide.style.visibility = "visible";
           } 
-}
+};
+
 
 function start() {
     currentQ = 0;
@@ -57,7 +65,7 @@ function start() {
     document.getElementById("question").innerHTML = "<p>Click the button below to begin!</p></br><button id='button'>Start Quiz!</button>";
     document.getElementById("button").addEventListener("click", timerStart);
     document.getElementById("button").addEventListener("click", getQuiz);
-    document.getElementById("button").addEventListener("click", hideAnswers);
+    document.getElementById("button").addEventListener("click", showAnswers);
     return;
 };
 
@@ -84,6 +92,7 @@ function checkAnswers(e) {
 
     if (selectedOption.textContent === questions[currentQ].a) {
         correct = "Correct!"
+        userScore += 10;
     } else {
         correct = "Wrong!";
         timer -= 10;
@@ -93,6 +102,17 @@ function checkAnswers(e) {
     return getQuiz();
 };
 
+function highScore (){
+    var username = document.getElementById("text1").value;
+
+    score.push([userScore, username]);
+
+    window.localStorage.setItem("score", JSON.stringify(score));
+
+    var storedScores = JSON.parse(window.localStorage.getItem("score"));
+        
+    document.getElementById('disp').innerHTML= storedScores;
+};
 
 
 start();
